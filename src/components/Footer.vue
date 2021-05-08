53a6686fd2f2f8807c00d4539e724e3941f349e5
Sofia Sinani <sofsin1208@gmail.com>
2021-05-08 19:15:17 +0200

    Edited Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -18,16 +18,15 @@ name: "Footer"
 
 <style scoped>
 .footer {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   position: absolute;
   left: 0;
   bottom: 0;
   width: 100%;
-  background-color: #B3B3B3;
   height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #B3B3B3;
 }
 
 li {
