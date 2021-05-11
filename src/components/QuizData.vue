def6d525701dc04dc623f3b8d70ce250c4e5300a
JannisMueller <jannis.mueller@hotmail.de>
2021-05-11 10:24:39 +0200

    logic of game

--- src/components/QuizData.vue
+++ src/components/QuizData.vue
@@ -1,38 +1,74 @@
 <template>
   <div>
     <ul>
-      <li v-for="question in questions"
+      <li v-for="(question,index) in questions"
           v-bind:key="question.questionId"
-          v-bind:answer="question.answerOne"
+
           class="question_grid">
+
+        <div v-show="index === questionIndex">
+
         <h1>{{question.questionTitle}}</h1>
         <h2>{{question.question}}</h2>
+
         <form class="radio_button_grid">
-          <label for="input_answer_one">{{question.answerOne}}</label>
-          <input id="input_answer_one" type="radio">
-          <label for="input_answer_two">{{question.answerTwo}}</label>
-          <input id="input_answer_two" type="radio">
-          <label for="input_answer_three">{{question.answerThree}}</label>
-          <input id="input_answer_three" type="radio">
+
+          <input type="radio" id="one" v-bind:value="question.answerOne" v-model="picked">
+          <label for="one">{{question.answerOne}}</label>
+
+          <input type="radio" id="two" v-bind:value="question.answerTwo" v-model="picked">
+          <label for="two">{{question.answerTwo}}</label>
+
+          <input type="radio" id="three" v-bind:value="question.answerThree" v-model="picked">
+          <label for="three">{{question.answerThree}}</label>
+
+          <span >Your Answer is: {{ picked }} </span>
+
         </form>
+        </div>
       </li>
     </ul>
-    <button  @click="correctAnswers" type="submit">Submit</button>
+    <button v-if="questionIndex > 0" v-on:click="prev">
+      prev
+    </button>
+
+    <button v-on:click="next">
+      next
+    </button>
+
+<!--    -->
   </div>
 </template>
 <script>
 export default {
   name: "QuizData",
+
   data: function () {
     return {
       questions: [],
+      questionIndex: 0,
+      picked:'',
+      points: 0
     }
   },
   methods: {
-    correctAnswer: function () {
 
+    next: function() {
+      this.questionIndex++;
+      let correctedAnswer = this.questions[this.questionIndex-1].correctAnswer
+      if(this.picked === correctedAnswer){
+        this.points++;
+      }
+      console.log("question Index: " + this.questionIndex)
+      console.log("picked: " + this.picked)
+      console.log("points: " + this.points)
+      console.log("correct Answer: " + correctedAnswer)
+      this.picked='';
 
-    }
+    },
+    prev: function() {
+      this.questionIndex--;
+    },
 
   },
   mounted(){
