c75e95bd822d356ee7bcdaeb9225783b40054ebc
Kozue Yamada <kozue.yamada@iths.se>
2021-05-17 11:59:07 +0200

    Added one component + one json file & edited References

---
+++ src/components/ReferenceList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button v-on:click="getReferenceData">Get Reference Data</button>
+    <div>{{ referenceDataList }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReferenceList",
+  data() {
+    return {
+      referenceDataList: []
+    };
+  },
+  methods: {
+    getReferenceData() {
+      fetch('./assets/reference-list.json')
+          .then(response => response.json())
+          .then(data => (this.referenceDataList = data));
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/References.vue
+++ src/views/References.vue
@@ -1,15 +1,53 @@
 <template>
-  <div>
-    References
+  <div id="app">
+    <h2>References</h2>
+    <ReferenceList />
   </div>
 </template>
 
 <script>
+import ReferenceList from "@/components/ReferenceList";
+
 export default {
-  name: 'References'
+  name: 'app',
+  components: {
+    ReferenceList
+  }
 }
 </script>
 
 <style scoped>
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h2 {
+  color: var(--color-text-primary);
+  margin: 30px 0 30px 0;
+}
+
+/*figure {*/
+/*  width: 250px;*/
+/*  height: 250px;*/
+/*  background-image: url("../assets/about-illustration.png");*/
+/*  background-size: cover;*/
+/*  margin-bottom: 20px;*/
+/*}*/
+
+/*#text {*/
+/*  color: var(--color-text-secondary);*/
+/*  max-width: 500px;*/
+/*  margin: 0 30px 50px 30px;*/
+/*}*/
+
+a {
+  text-decoration: none;
+  font-weight: bold;
+  color: var(--color-bg-button);
+}
 
 </style>
