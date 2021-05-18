c7d902647621c3eb596d3f2c8c0f14c81c640ed9
Sofia Sinani <sofsin1208@gmail.com>
2021-05-18 09:35:53 +0200

    Edited references and referencelist

--- src/components/ReferenceList.vue
+++ src/components/ReferenceList.vue
@@ -1,28 +1,86 @@
 <template>
-  <div>
-    <button v-on:click="getReferenceData">Get Reference Data</button>
-    <div>{{ referenceDataList }}</div>
+  <div class="reference_flex">
+    <h2>References</h2>
+    <button @click="toggleReferences" >Get References</button>
+    <ul
+        :class="{ active: isActive }"
+    >
+      <li
+          v-for="referenceData in referenceDataList"
+          :key="referenceData.referenceId"
+      >
+        {{ referenceData.referenceName }}
+        <a :href="referenceData.referenceURL" target="_blank">{{ referenceData.referenceURL }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import referenceList from '@/assets/reference-list.json'
+
 export default {
   name: "ReferenceList",
   data() {
     return {
-      referenceDataList: []
-    };
+      referenceDataList: referenceList,
+      isActive: false
+    }
   },
+
   methods: {
-    getReferenceData() {
-      fetch('./assets/reference-list.json')
-          .then(response => response.json())
-          .then(data => (this.referenceDataList = data));
+    toggleReferences() {
+      this.isActive = !this.isActive;
     }
   }
 }
 </script>
 
 <style scoped>
+.reference_flex {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h2 {
+  margin: 30px 0 30px 0;
+  color: var(--color-text-primary);
+}
+
+button {
+  padding: 20px;
+  border: 0;
+  border-radius: 10px;
+  color: var(--color-text-button);
+  background-color: var(--color-bg-button);
+  font-size: 18px;
+  font-weight: bold;
+  letter-spacing: 2px;
+  margin-bottom: 50px;
+  cursor: pointer;
+  transition: background-color 500ms ease-in-out;
+}
+
+button:hover {
+  background-color: var(--color-text-secondary);
+}
+
+ul {
+  display: none;
+}
+ul.active {
+  display: block;
+}
+li {
+  color: var(--color-text-secondary);
+  list-style-type: none;
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+a {
+  color: var(--color-bg-button);
+}
 
 </style>
--- src/views/References.vue
+++ src/views/References.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <h2>References</h2>
+  <div class="references">
     <ReferenceList />
   </div>
 </template>
@@ -17,37 +16,5 @@ export default {
 </script>
 
 <style scoped>
-
-#app {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-h2 {
-  color: var(--color-text-primary);
-  margin: 30px 0 30px 0;
-}
-
-/*figure {*/
-/*  width: 250px;*/
-/*  height: 250px;*/
-/*  background-image: url("../assets/about-illustration.png");*/
-/*  background-size: cover;*/
-/*  margin-bottom: 20px;*/
-/*}*/
-
-/*#text {*/
-/*  color: var(--color-text-secondary);*/
-/*  max-width: 500px;*/
-/*  margin: 0 30px 50px 30px;*/
-/*}*/
-
-a {
-  text-decoration: none;
-  font-weight: bold;
-  color: var(--color-bg-button);
-}
 
 </style>
